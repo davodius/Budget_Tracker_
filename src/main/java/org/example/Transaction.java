9edb64f802e5c849b929b7d7daeda293b81e27be
davodius <davidhanebrant@gmail.com>
2023-10-26 09:57:48 +0200

    added heritage in transaction

--- src/main/java/org/example/Transaction.java
+++ src/main/java/org/example/Transaction.java
@@ -2,8 +2,23 @@ package org.example;
 
 public class Transaction {
 
+private String id;
+private String date;
+private String amount;
 
 
+    public Transaction(String id, String date, String amount) {
+        this.id = id;
+        this.date = date;
+        this.amount = amount;
+    }
 
-
+    @Override
+    public String toString() {
+        return "Transaction{" +
+                "id='" + id + '\'' +
+                ", date='" + date + '\'' +
+                ", amount='" + amount + '\'' +
+                '}';
+    }
 }
